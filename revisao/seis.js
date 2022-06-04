44ff014a66a07e910fe90e2346ba66c01f49e4b2
viviane <vivianedasilvasouza2020@gmail.com>
2022-06-04 12:00:39 -0300

    atividade for

---
+++ revisao/seis.js
@@ -0,0 +1,16 @@
+/* let nomeNave = prompt('qual nome da sua nave?')
+let subistituir = prompt('qual caractere deseja substituir')
+let trocar = prompt('por qual caractere deseja substituir?')
+let nova = ''
+
+for (let indice = 0; indice < nomeNave.length; indice++) {
+    if (nomeNave[indice] == subistituir) {
+        nova += trocar
+    } else {
+        nova += nomeNave[indice]
+    }
+} */
+
+for (let i = 1; i <= 5; i++) { //(começo; condição; execução)
+    console.log('estou aprendendo!', i)
+}
