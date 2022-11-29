b4d2faeb51cc5bcda94a19c505e4adeb3cb71673
viviane <vivianedasilvasouza2020@gmail.com>
2022-11-29 07:35:38 -0300

    refactor: delete de atividade

--- aula4.js
+++
@@ -1,4 +0,0 @@
-let vigador = "thor"
-let vigador2 = "homen de ferro "
-
-console.log(vigador2 != "90 ")
--- for.js
+++
@@ -1,6 +0,0 @@
-let spaceship = 'Helmet'
-let newSpaceship = ''
-
-for (let i = 0; i < spaceship.length; i++) {
-    console.log(spaceship[i])
-}
